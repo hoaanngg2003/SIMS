45660bae39af2f0234a8683ebe3440b8b69edce1
Tran Cong Hoang <tranhoang132003@gmail.com>
2024-09-25 13:00:00 +0700

    Done 70% Commit

---
+++ SIMS_Application/Models/CourseModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SIMS_Application.Models
+{
+    public class CourseModel
+    {
+        public int IdCourse { get; set; }
+        public string NameCourse { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string DescriptionCourse { get; set; }
+        public string Schedule {  get; set; }
+        public CourseModel() 
+        { 
+
+        }
+
+    }
+}
